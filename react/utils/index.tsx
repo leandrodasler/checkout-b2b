5176de66b3c66e0f085116484a2b528015f72c29
Icaro Oliveira <icarovinici@gmail.com>
2024-10-03 18:54:46 -0300

    feat: adds address on totalizers

--- react/utils/index.tsx
+++ react/utils/index.tsx
@@ -1,15 +1,28 @@
 import React from 'react'
-import { Totalizer } from 'vtex.checkout-graphql'
+import { OrderForm, Totalizer } from 'vtex.checkout-graphql'
 import { FormattedPrice } from 'vtex.formatted-price'
 
-export function getTotalizers(totalizers: Totalizer[], total: number) {
+export function getTotalizers(
+  totalizers: Totalizer[],
+  shipping: OrderForm['shipping'],
+  total: number
+) {
   if (!totalizers.length) return null
+
+  const formattedAddress = shipping?.selectedAddress
+    ? `${shipping.selectedAddress.street}, Nº ${shipping.selectedAddress.number} - ${shipping.selectedAddress.city}, ${shipping.selectedAddress.state}`
+    : 'Endereço não informado'
 
   return [
     ...totalizers.map((t) => ({
       label: t.name,
       value: <FormattedPrice value={t.value / 100} />,
     })),
+    {
+      label: 'Endereço de entrega',
+      value: formattedAddress,
+      isLoading: false,
+    },
     {
       label: 'Total',
       value: <FormattedPrice value={total / 100} />,
