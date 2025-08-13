1e1b6af35cf5fa43c26ec826b98b567d48b30c53
Tiago de Andrade Freire <tiago.freire@cubos.io>
2025-08-13 19:46:32 -0300

    refactor: removing try catch from saveRepresentativeBalance

--- node/resolvers/mutations/saveRepresentativeBalance.ts
+++ node/resolvers/mutations/saveRepresentativeBalance.ts
@@ -26,66 +26,60 @@ export const saveRepresentativeBalance = async (
   },
   context: ServiceContext<Clients>
 ) => {
-  try {
-    await saveSchemas(context)
+  await saveSchemas(context)
 
-    const inputEmail = await getRepresentativeEmail(context, email)
-    const { masterdata } = context.clients
+  const inputEmail = await getRepresentativeEmail(context, email)
+  const { masterdata } = context.clients
 
-    const representativeBalance = await getRepresentativeBalanceByEmail(
-      null,
-      { email: inputEmail },
-      context
-    )
+  const representativeBalance = await getRepresentativeBalanceByEmail(
+    null,
+    { email: inputEmail },
+    context
+  )
 
-    const oldBalance = representativeBalance?.balance ?? 0
-    const newBalance = overwrite ? balance : oldBalance + balance
-    let representativeBalanceId = representativeBalance?.id
+  const oldBalance = representativeBalance?.balance ?? 0
+  const newBalance = overwrite ? balance : oldBalance + balance
+  let representativeBalanceId = representativeBalance?.id
 
-    if (typeof balance === 'number') {
-      const { DocumentId } = await masterdata.createOrUpdateEntireDocument({
-        dataEntity: REPRESENTATIVE_BALANCE_ENTITY,
-        fields: {
-          email: inputEmail,
-          balance: newBalance,
-        },
-        schema: SCHEMA_VERSION,
-        id: representativeBalance?.id,
-      })
-
-      representativeBalanceId = DocumentId
-    }
-
-    if (!representativeBalanceId) {
-      throw new NotFoundError('representative-balance-not-found')
-    }
-
-    await masterdata.createDocument({
-      dataEntity: REPRESENTATIVE_BALANCE_TRANSACTION_ENTITY,
+  if (typeof balance === 'number') {
+    const { DocumentId } = await masterdata.createOrUpdateEntireDocument({
+      dataEntity: REPRESENTATIVE_BALANCE_ENTITY,
       fields: {
         email: inputEmail,
-        oldBalance,
-        newBalance,
-        orderGroup,
+        balance: newBalance,
       },
       schema: SCHEMA_VERSION,
-      ...(representativeBalanceId !== inputEmail && {
-        id: representativeBalanceId,
-      }),
-
+      id: representativeBalance?.id,
     })
 
-    const updatedRepresentativeBalance = await masterdata.getDocument<RepresentativeBalance | null>(
-      {
-        dataEntity: REPRESENTATIVE_BALANCE_ENTITY,
-        fields: REPRESENTATIVE_BALANCE_FIELDS,
-        id: representativeBalanceId,
-      }
-    )
-
-    return updatedRepresentativeBalance
-  } catch (error) {
-    console.error('Erro saveRepresentativeBalance:', error)
-    throw error
+    representativeBalanceId = DocumentId
   }
+
+  if (!representativeBalanceId) {
+    throw new NotFoundError('representative-balance-not-found')
+  }
+
+  await masterdata.createDocument({
+    dataEntity: REPRESENTATIVE_BALANCE_TRANSACTION_ENTITY,
+    fields: {
+      email: inputEmail,
+      oldBalance,
+      newBalance,
+      orderGroup,
+    },
+    schema: SCHEMA_VERSION,
+    ...(representativeBalanceId !== inputEmail && {
+      id: representativeBalanceId,
+    }),
+  })
+
+  const updatedRepresentativeBalance = await masterdata.getDocument<RepresentativeBalance | null>(
+    {
+      dataEntity: REPRESENTATIVE_BALANCE_ENTITY,
+      fields: REPRESENTATIVE_BALANCE_FIELDS,
+      id: representativeBalanceId,
+    }
+  )
+
+  return updatedRepresentativeBalance
 }
